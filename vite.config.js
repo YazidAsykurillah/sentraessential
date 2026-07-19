21ea519d9bf4abbeb979c058495dc54314e7411c
YazidAsykurillah <yasykurillah@gmail.com>
2026-07-19 23:28:14 +0700

    feat:fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'cms.local',
+            host: 'sentraessential.local',
         },
         watch: {
             ignored: ['**/storage/framework/views/**'],
